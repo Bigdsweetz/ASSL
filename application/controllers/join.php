bbb0836c314a69f0cd8799694cc931c754bb6a77
Bigdsweetz <bigdsweetz@gmail.com>
2015-01-14 04:29:59 -0500

    Adding header and footer
    I’m adding a header and footer to the page. this way all I will have
    left is simply creating the forms for the individual pages.

--- application/controllers/join.php
+++ application/controllers/join.php
@@ -1,12 +1,19 @@
 <?php 
 
-
 class Join extends CI_Controller 
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        
+    }
+    
     public function index()
     {
+        $this->load->view('header');
         echo "Hello from Space";
+        $this->load->view('footer');
     }
 
 }
