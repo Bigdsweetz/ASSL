e9f15a315a19f749d2c88c088319006a04f44246
Bigdsweetz <bigdsweetz@gmail.com>
2015-01-16 23:32:53 -0500

    Phase 1
    I’ve made a back up of my pages so now I’m creating my actual project.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,37 +1,11 @@
- </div>
-  </div>
-  <div id="footer">
-  <div id="footerMenu">
-  <h3>Quick Nav</h3>
-    <ul id="quickNav">
-      <li><a href="http://explorecalifornia.org/tours.htm" title="Explore our tours">Tours</a></li>
-      <li><a href="http://explorecalifornia.org/mission.htm" title="What we think">Mission</a></li>
-      <li><a href="http://explorecalifornia.org/contact.htm" title="Contact and support">Contact</a></li>
-      <li><a href="http://explorecalifornia.org/resources.htm" title="Guidance and planning">Resources</a></li>
-      <li><a href="http://explorecalifornia.org/explorers.htm" title="Join our community">Explorers</a></li>
-    </ul>
-</div>
-  <div id="footerBody">
-  <div id="footerMidCol">
-      <h3><a href="http://explorecalifornia.org/resources/faq.htm">FAQ</a></h3>
-      <p>Find out more about our tours</p>
-      <h3><a href="http://explorecalifornia.org/contact.htm">Support</a></h3>
-      <p>Need help? It's just a click away!</p>
-      <h3><a href="http://explorecalifornia.org/explorers/join.htm">Legal Disclaimer</a></h3>
-    </div>
-    <div id="footerRightCol">
-      <h1>Explore California</h1>
-      <p>5605 Nota Street<br>
-        Ventura, CA 93003</p>
-      <p class="phone">866.555.4310</p>
-    </div>
-</div>
-</div>
-</div>
-<script type="text/javascript">
-var sprypassword1 = new Spry.Widget.ValidationPassword("sprypassword1", {minChars:6, validateOn:["blur", "change"]});
-var spryconfirm1 = new Spry.Widget.ValidationConfirm("spryconfirm1", "pwd");
-</script>
+  <div class="row">
+                <div class="span12">
+                    <div class="well">
+                        <center><H3>Copyright &copy; DayBreak Journey Studio</H3></center>
+                    </div>
+                </div>
+            </div>
 
-
-</body></html>
+</div>
+  </body>
+</html>
---
+++ application/views/navbar.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="span12">
+        <ul class="nav nav-tabs">
+            <li><a href="#">HOME</A></li>
+            <li><a href="#">PROFILE</A></li>
+            <li><a href="#">CHAT</A></li>
+            <li><a href="#">FORUM</A></li>
+        </ul>
+    </div>
+</div>
