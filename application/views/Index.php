66a16fd445031a4db383d531e1ab97a3eff8e6b0
Bigdsweetz <bigdsweetz@gmail.com>
2015-01-17 21:38:45 -0500

    Almost done
    Created the Index page, Register page, Chat page, Forum page

---
+++ application/views/Index.php
@@ -0,0 +1,12 @@
+  <div class="row">
+                <div class="span12">
+                    <div class="well">
+                        <center><H3>Please sign in or Register/H3></center>
+                            <a href='#'>Sign In</a><h1> or </h1> <a href='#'>Register</a> 
+                    </div>
+                </div>
+            </div>
+
+</div>
+  </body>
+</html>
---
+++ application/views/register.php
@@ -0,0 +1,11 @@
+  <div class="row">
+                <div class="span12">
+                    <div class="well">
+                        <center><H3>Copyright &copy; DayBreak Journey Studio</H3></center>
+                    </div>
+                </div>
+            </div>
+
+</div>
+  </body>
+</html>
