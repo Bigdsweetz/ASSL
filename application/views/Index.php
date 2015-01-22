3762cb0113963e71d3b4d85b51f6355b8ddba744
Bigdsweetz <bigdsweetz@gmail.com>
2015-01-22 03:48:48 -0500

    Nav bar updated
    The navigation bar has been updated

--- application/views/Index.php
+++ application/views/Index.php
@@ -2,7 +2,7 @@
                 <div class="span12">
                     <div class="well">
                         <center><H3>This is the main page</H3></center>
-                            <center><a href='#'>Sign In</a><h1> or </h1> <a href='#'>Register</a> </center>
+                            <center><a href='<?php echo site_url('LoginController'); ?>'>Sign In</a><h1> or </h1> <a href='<?php echo site_url('RegisterController'); ?>'>Register</a> </center>
                     </div>
                 </div>
             </div>
