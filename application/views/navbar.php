28dbdcb666f731ec9a874233b41ce1c644ef459f
Bigdsweetz <bigdsweetz@gmail.com>
2015-01-28 01:38:18 -0500

    Log in works
    So I have the accounts created. I also have the admin program loaded.
    I’m using appoint auth for that.  I’ve also got it to load most of my
    pages.

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -2,10 +2,10 @@
     <div class="span12">
         
             <ul class="nav nav-tabs">
-            <li><a href="<?php echo site_url('IndexController'); ?>">HOME</A></li>
-            <li><a href="<?php echo site_url('HomeController'); ?>">PROFILE</A></li>
-            <li><a href="<?php echo site_url('ChatController'); ?>">CHAT</A></li>
-            <li><a href="<?php echo site_url('BlogController'); ?>">BLOG</A></li>
+            <li><a href="<?php echo site_url('indexcontroller'); ?>">HOME</A></li>
+            <li><a href="<?php echo site_url('Homecontroller'); ?>">PROFILE</A></li>
+            <li><a href="<?php echo site_url('chatcontroller'); ?>">CHAT</A></li>
+            <li><a href="<?php echo site_url('blogcontroller'); ?>">BLOG</A></li>
             </ul>
         
     </div>
